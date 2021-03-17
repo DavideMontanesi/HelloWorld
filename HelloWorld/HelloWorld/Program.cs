b1ed61a7da9fabe4162e8bac32b41c32443d2ee3
Davide Montanesi <davide.montanesi@gmail.com>
2021-03-17 19:53:54 +0100

    aggiunto metodo printLogo

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,12 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            printLogo();
         }
+        static void printLogo()
+        {
+            Console.WriteLine("UNIGE");
+        }
+
     }
 }
